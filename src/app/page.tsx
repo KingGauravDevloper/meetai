10489075a122d603c82a6098b191bca5578caf11
KingGauravDeveloper <kinggaurav.official@gmail.com>
2025-08-15 14:39:57 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
-import Image from "next/image";
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = () => {
+    authClient.signUp.email({
+      email, name, password,
+    },
+  {
+    onError: () => {
+      window.alert("Something wrong");
+    },
+    onSuccess: () => {
+      window.alert("Sucess");
+    }
+  })
+  }
+
   return (
-    <div className="text-4xl font-bold text-green-400"> Hello  meet </div>
+    <div className="p-4 flex flex-col gap-y ">
+      <input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+      <input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <input placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+    
+
+    <button onClick={onSubmit}>
+      Create User
+    </button>
+   </div>
   );
 }
